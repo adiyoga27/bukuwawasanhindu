25a5669d1895fd992b95a4fb15c5261d11ad986a
CodingAja <codingajaindonesia@gmail.com>
2025-07-31 15:09:58 +0000

    add

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -370,6 +370,36 @@
                 height: 200px;
             }
         }
+
+        
+.whatsapp-float {
+    position: fixed;
+    width: 60px;
+    height: 60px;
+    bottom: 20px;
+    right: 20px;
+    background-color: #25d366;
+    color: white;
+    border-radius: 50px;
+    text-align: center;
+    font-size: 30px;
+    box-shadow: 2px 2px 5px rgba(0,0,0,0.3);
+    z-index: 1000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease-in-out;
+     text-decoration: none;
+}
+
+.whatsapp-float:hover {
+    transform: scale(1.1);
+    background-color: #1ebc59;
+}
+
+.whatsapp-icon {
+    margin-top: 2px;
+}
     </style>
     
     @yield('css')
@@ -459,7 +489,10 @@
             </div>
         </div>
     </footer>
-
+<!-- Floating WhatsApp Button -->
+<a href="https://wa.me/{{ $configs->phone }}" class="whatsapp-float" target="_blank">
+ <i class="fab fa-whatsapp"></i>
+</a>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     
