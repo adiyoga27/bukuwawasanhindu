d25cadf03ecf2b5f9d3d0767c4ee9cefb7b03e93
CodingAja <codingajaindonesia@gmail.com>
2025-07-31 05:17:59 +0000

    fixed

--- resources/views/contents/guest/article-detail.blade.php
+++ resources/views/contents/guest/article-detail.blade.php
@@ -8,9 +8,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-       <link rel="canonical" href="{{ url('articles/'.$article->slug) }}">
+    <link rel="canonical" href="{{ url('articles/'.$article->slug) }}">
+    
     <meta name="description" content="{{ Str::limit(strip_tags($article->content), 225, '...') }}">
-    <meta name="keywords" content="Ebook, SantoBook, Kandapat">
+    <meta name="keywords" content="{{ $article->meta_keywords }}">
     <meta name="author" content="{{ $article->author }}">
     <meta name="image" content="{{ url('storage/') }}/{{ $article->featured_image }}">
     <meta name="title" content="{{ $article->title }} ">
